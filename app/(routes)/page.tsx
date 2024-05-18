35d86d19b1b0aef6728b8e3fe4485df18af5eef5
Okoji Kelechi <kelechiokoji52@gmail.com>
2024-05-18 19:07:09 +0100

    test pipeline

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,7 @@ import FeaturedProjects from "@/components/featured-projects";
 import Footer from "@/components/footer";
 import HeroSection from "@/components/hero-section";
 
+//test
 export default async function Home() {
   return (
     <div>
